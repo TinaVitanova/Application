332f4f5e79149c71b0a144d152972c81dc27cd6d
kbojadziski <bojadziski4@gmail.com>
2017-09-25 16:47:08 +0200

    666

--- src/pages/reserve/reserve.ts
+++ src/pages/reserve/reserve.ts
@@ -1,6 +1,6 @@
-import { prepareWatcher } from '@ionic/app-scripts/dist/watch';
+import { ViewController } from 'ionic-angular/es2015';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams} from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import * as moment from 'moment';
 /**
@@ -75,7 +75,7 @@ export class ReservePage {
       this.navCtrl.push(ReservePage, {date: this.myDate, time: this.myTime, capacity: this.capacity, room: this.rooms});
   }
   
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public viewCtrl: ViewController ) {
     let preselectedDate = moment(this.navParams.get('selectedDay')).format();
     this.event.statTime = preselectedDate;
     this.event.endTime = preselectedDate;
