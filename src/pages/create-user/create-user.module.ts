b8bb33da390f02ade48eb2f4ddae24d1ce37000d
TinaVitanova <tina.vitanova@outlook.com>
2017-10-06 16:14:56 +0200

    my profile image

--- src/pages/create-user/create-user.module.ts
+++ src/pages/create-user/create-user.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CreateUserPage } from './create-user';
-import { FileUploaderModule } from "file-uploader-component/angular";
+
 
 @NgModule({
   declarations: [
     CreateUserPage,
   ],
-  imports: [
-    FileUploaderModule,
+  imports: [,
     IonicPageModule.forChild(CreateUserPage),
   ],
 })
